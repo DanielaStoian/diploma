4f890068b2eb01a2365d349f44a9ee38fa2e758f
Daniela Stoian <dstoian@epu.ntua.gr>
2023-11-27 16:27:28 +0200

    production added

--- backend/populate_dhmoi.py
+++ backend/populate_dhmoi.py
@@ -7,4 +7,4 @@ from charging_stations.models import *
 
 organized_dhmoi = data = pd.read_csv('./scrapper/stations_scrapper/stations_scrapper/spiders/organized_dhmoi.csv')
 for i in range(0,len(organized_dhmoi)):
-    Dhmos.objects.create(name = organized_dhmoi['ΚΑΛΛΙΚΡΑΤΙΚΟΙ ΔΗΜΟΙ'][i], category_name = organized_dhmoi['ΧΑΡΑΚΤΗΡΙΣΜΟΣ ΔΗΜΟΥ'][i], category = organized_dhmoi['category'][i])
+    Dhmos.objects.get_or_create(name = organized_dhmoi['ΚΑΛΛΙΚΡΑΤΙΚΟΙ ΔΗΜΟΙ'][i], category_name = organized_dhmoi['ΧΑΡΑΚΤΗΡΙΣΜΟΣ ΔΗΜΟΥ'][i], category = organized_dhmoi['category'][i])
