1d644ccff5cd07f039b4f40c947784ddf8e81880
Daniela Stoian <dstoian@epu.ntua.gr>
2023-11-27 21:47:53 +0200

    production added

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -34,7 +34,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    # 'django.contrib.staticfiles',
     'charging_stations',
     'corsheaders',
     'rest_framework'
@@ -111,6 +111,7 @@ CORS_ALLOW_HEADERS = [
 ]
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000',
+    'https://ev-chain.epu.ntua.gr'
 )
 
 # Password validation
@@ -147,7 +148,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-# STATIC_URL = 'static/'
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
