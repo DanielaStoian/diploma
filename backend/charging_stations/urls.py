de0aa0b4dd1b9849f51c0005215283e540a496da
Daniela Stoian <dstoian@epu.ntua.gr>
2023-11-27 20:28:35 +0200

    production added

--- backend/charging_stations/urls.py
+++ backend/charging_stations/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from charging_stations.views import StationsView, ProfilesView
+from django.views.generic.base import TemplateView
 
 router = routers.DefaultRouter()
 router.register(r'stations', StationsView, basename="stations")
@@ -10,4 +11,5 @@ router.register(r'profiles', ProfilesView, basename="profiles")
 
 urlpatterns = [
    path('', include(router.urls)),
+   path('manifest.json', TemplateView.as_view(template_name='manifest.json', content_type='application/json'), name='manifest.json')
 ]
