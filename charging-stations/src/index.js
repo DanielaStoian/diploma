68e97af90d6ee4e8ad4c790a01dc07d46ee0ab3e
Daniela Stoian <dstoian@epu.ntua.gr>
2023-12-05 17:04:26 +0200

    basename added

--- charging-stations/src/index.js
+++ charging-stations/src/index.js
@@ -11,7 +11,7 @@ import store from './redux/store'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/app2'>
     <Provider store={store}>
     <App />
     </Provider>
